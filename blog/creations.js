3390b1b6c372f3ad81f30c8bf111d0e8062d4f32
Aashi Agrawal <aashiag2003@gmail.com>
2024-03-27 18:35:12 +0530

    Added My Blogs Page

---
+++ blog/creations.js
@@ -0,0 +1,35 @@
+window.addEventListener("load", function () {
+    const target = document.querySelector("#blogs-container");
+    const blogs = JSON.parse(localStorage.getItem("blogs")) || [];
+    const user = JSON.parse(localStorage.getItem("current-user")) || null;
+    if(!user){
+        window.location.pathname = '../login.html';
+        return;
+    }
+    if (blogs.filter(e => e.authorId === user.uid).length !== 0) {
+      target.innerHTML = ``;
+    }
+    blogs.filter(e => e.authorId === user.uid).forEach((element) => {
+      target.innerHTML += `<div class="card w-96 bg-base-100 shadow-xl transition-all hover:scale-[1.05] cursor-pointer"  onclick="viewBlog(${element.blogId})">
+          <figure class="w-96 h-[256px]"><img data-tag="${element.tag}" class="max-h-[256px] h-full object-cover" src="${element.bannerImage}" onerror="imageError(this)" /></figure>
+          <div class="card-body">
+            <h2 class="card-title">
+              ${element.title}
+            </h2>
+            <div class="badge badge-secondary">${element.tag}</div>
+            <p class="text-xl font-medium" >${element.authorName}</p>
+          </div>
+        </div>`;
+    });
+  });
+  
+  function imageError(target) {
+    target.src = `https://source.unsplash.com/random/384x256/?${
+      target.dataset.tag ?? "blog"
+    }`;
+  }
+
+  function viewBlog(id){
+    localStorage.setItem('view-blog', id);
+    window.location.pathname = 'blog/view.html';
+  }
--- main.js
+++ main.js
@@ -5,7 +5,7 @@ window.addEventListener("load", function () {
     target.innerHTML = ``;
   }
   blogs.forEach((element) => {
-    target.innerHTML += `<div class="card w-96 bg-base-100 shadow-xl transition-all hover:scale-[1.05] cursor-pointer">
+    target.innerHTML += `<div class="card w-96 bg-base-100 shadow-xl transition-all hover:scale-[1.05] cursor-pointer" onclick="viewBlog(${element.blogId})" >
         <figure class="w-96 h-[256px]"><img data-tag="${element.tag}" class="max-h-[256px] h-full object-cover" src="${element.bannerImage}" onerror="imageError(this)" /></figure>
         <div class="card-body">
           <h2 class="card-title">
@@ -19,7 +19,11 @@ window.addEventListener("load", function () {
 });
 
 function imageError(target) {
-  target.src = `https://source.unsplash.com/random/?${
+  target.src = `https://source.unsplash.com/random/384x256/?${
     target.dataset.tag ?? "blog"
   }`;
+}
+function viewBlog(id){
+  localStorage.setItem('view-blog', id);
+  window.location.pathname = 'blog/view.html';
 }
